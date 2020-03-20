006e9c78e9acfecb43fa170161187f0fc283edc0
laurent naitza <lolonaitza@gmail.com>
2020-03-20 01:39:36 +0100

    first commit config react OK Devise OK Cw cloudniary OK

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/edit'
+  get 'articles/delete'
+  root "pages#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
