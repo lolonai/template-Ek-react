c52e00a01eaf6679049139f27e8f28a0f62a5c43
laurent naitza <lolonaitza@gmail.com>
2020-03-29 22:53:09 +0200

    add react router and link

--- app/javascript/containers/Home.js
+++ app/javascript/containers/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="home-container">
+    <div className="home-container">
       <h1>Salut, bienvenue sur ce template !</h1>
     </div>
   );
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'articles/edit'
   get 'articles/delete'
   root 'pages#index'
+
+  match '*path', to: 'pages#index', via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
